a8a79e2d6c75d20a86639c0d96d061789aeef8fa
wa451 <fukuda.wataru.067@s.kyushu-u.ac.jp>
2023-03-16 22:27:08 +0900

    mainボタンクリックIntent

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.passwordmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNew: Button = findViewById(R.id.btnNew)
+        btnNew.setOnClickListener {
+            val intent = Intent(this,EditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
